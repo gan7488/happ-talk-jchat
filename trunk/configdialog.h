2656103c784e370e1646b835dea458b479abe208
zversergey@tut.by <zversergey@tut.by@f91e8598-1b5a-aa9d-f05f-0f4e52466c13>
2011-03-05 18:09:34 +0000

    About Dialog: лого грузить с ресурсов надо будет описание само собой а, так закончено
    Other files:
    усердно трудиться! там много работ

---
+++ trunk/configdialog.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+**	Author: Svirskiy Sergey			Nickname: Happ
+******************************************************************************/
+
+#ifndef CONFIGDIALOG_H
+#define CONFIGDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class ConfigDialog;
+}
+
+class ConfigDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ConfigDialog(QWidget *parent = 0);
+    ~ConfigDialog();
+
+private:
+    Ui::ConfigDialog *ui;
+};
+
+#endif // CONFIGDIALOG_H
--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,3 +1,7 @@
+/******************************************************************************
+**	Author: Svirskiy Sergey			Nickname: Happ
+******************************************************************************/
+
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
@@ -9,3 +13,21 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
+
+/*
+HOWTO: Styles
+
+    QFile file(":/qss/" + sheetName.toLower() + ".qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+
+    ui.styleTextEdit->setPlainText(styleSheet);
+    qApp->setStyleSheet(styleSheet);
+*/
+
+/*
+ HOWTO: Langs
+QTranslator translator;
+     translator.load(QString("arrowpad_") + locale);
+     app.installTranslator(&translator);
+*/
